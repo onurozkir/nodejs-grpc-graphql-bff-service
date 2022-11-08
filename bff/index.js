dc0e35a49b83a6dcffedb7be96e9af136ba9e0eb
onur.ozkir <onur.ozkir@trendyol.com>
2022-11-09 01:35:20 +0300

    update index.js

--- bff/index.js
+++ bff/index.js
@@ -2,18 +2,6 @@ import main from './src/main'
 
 const app = {}
 
-async function gracefulExit() {
-    const { httpServer } = app
-
-    if (httpServer) httpServer.close()
-
-    process.exit(0)
-}
-
-;['SIGINT', 'SIGTERM', 'SIGHUP'].forEach((signal) => {
-    process.on(signal, gracefulExit)
-})
-
 main()
     .then((obj) => {
         Object.assign(app, obj)
